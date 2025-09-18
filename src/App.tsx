e4a629d3f6108d085022d80a5ecdb2b4828ef7fc
Igor Carvalho <igorcarvalho288@gmail.com>
2025-09-18 17:41:39 -0300

    feat: start use useQuery

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,18 @@
 
-import './App.css'
+import {useQuery} from '@tanstack/react-query'
 
-function App() {
-  return (
-    <h1>
-      Hello Word!
-    </h1>
-  )
+export function App() {
+
+    useQuery({
+        queryKey: ['users'],
+        queryFn: async () => {
+            const response = await fetch('http://localhost:3000/users')
+            return response
+        },
+
+    });
+
+    return (
+        <h1>Hello Word!</h1>
+    )
 }
-
-
-export default App
