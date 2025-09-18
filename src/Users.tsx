ffc892c1ce5e74f46f70025b42acb0ff4863e2c8
Igor Carvalho <igorcarvalho288@gmail.com>
2025-09-18 19:44:35 -0300

    feat: create function sleep and create button logar

--- src/Users.tsx
+++ src/Users.tsx
@@ -1,19 +1,29 @@
 import { useQuery } from "@tanstack/react-query";
 import type { IUser } from "./types"
-
+import sleep from "./sleep";
 export default function Users() {
-       const { data } = useQuery({
+
+       const { data, refetch, isFetching , isLoading} = useQuery({
+        enabled: false,
         queryKey: ['users'],
         queryFn: async (): Promise<IUser[]> => {
+            await sleep();
             const response = await fetch('http://localhost:3000/users')
             return response.json();
         },
-
     }, );
-
-
     return (
-              <div>
+         <div className="py-4">
+         <button type="button"
+         className="bg-white text-black px-4 py-2 rounded-lg"
+         onClick={() => refetch()}
+         >
+            Logar
+         </button>
+                {/* {teste de performarce e carregamento} */}
+                {isLoading && <h1>Carregando...</h1>}
+                {!isLoading && isFetching && <small>Fetching...</small>}
+
                 {data?.map(user => (
                     <div key={user.id}>
                     <strong className="block text-red-300">{user.name}</strong>
---
+++ src/sleep.ts
@@ -0,0 +1,5 @@
+export default function sleep(delay = 1500){
+    return new Promise(resolve => {
+        setTimeout(resolve, delay)
+    })
+}
